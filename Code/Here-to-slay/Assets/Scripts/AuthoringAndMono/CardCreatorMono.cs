5e80ba2cc1688f00e5afd2f79db35b9aa3ef6cce
Nguyen-Thac-Bach <129993123+Nguyen-Thac-Bach@users.noreply.github.com>
2024-12-01 19:28:54 +0100

    Update CardCreatorMono.cs
    next todo: implement bake method of HeroCardCreatorBaker

--- Code/Here-to-slay/Assets/Scripts/AuthoringAndMono/CardCreatorMono.cs
+++ Code/Here-to-slay/Assets/Scripts/AuthoringAndMono/CardCreatorMono.cs
@@ -1,6 +1,29 @@
+using Unity.Entities;
 using UnityEngine;
 
-public class CardCreatorMono
+namespace AuthoringAndMono
 {
-    
+    /// <summary>
+    /// Authoring component that exposes the HeroCardPrefab and Heroes_JSON_PATH to the Unity Editor.
+    /// Basically, this is needed so I can drag the prefabs, and set the JSON paths in the Unity Editor.
+    /// </summary>
+    public class CardCreatorAuthoring : MonoBehaviour
+    {
+        public GameObject HeroCardPrefab;
+        public string Heroes_JSON_PATH = "JSON/heroes";
+        //other prefabs and JSON paths will be added later
+    }
+
+    /// <summary>
+    /// Creates the entity from CardCreatorAuthoring data.
+    /// Needed to convert the data from the Unity Editor to ECS.
+    /// </summary>
+    public class CardCreatorBaker: Baker<CardCreatorAuthoring>
+    {
+        public override void Bake(CardCreatorAuthoring authoring)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
 }
+
