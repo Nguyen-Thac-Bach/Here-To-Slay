2d1a0c56f4dd8c11cb62fa565e3e3b8039041d52
Nguyen-Thac-Bach <129993123+Nguyen-Thac-Bach@users.noreply.github.com>
2024-12-02 15:32:38 +0100

    merge authoring and baking

--- Code/Here-to-slay/Assets/Scripts/AuthoringAndMono/CardCreatorAuthoring.cs
+++ Code/Here-to-slay/Assets/Scripts/AuthoringAndMono/CardCreatorAuthoring.cs
@@ -1,10 +1,12 @@
 using Unity.Entities;
+using Unity.Collections;
 using UnityEngine;
 
+using Components;
 namespace AuthoringAndMono
 {
     /// <summary>
-    /// Authoring component that exposes the HeroCardPrefab and Heroes_JSON_PATH to the Unity Editor.
+    /// Authoring component that exposes the "card prefabs" and "json file paths" to the Unity Editor.
     /// Basically, this is needed so I can drag the prefabs, and set the JSON paths in the Unity Editor.
     /// </summary>
     public class CardCreatorAuthoring : MonoBehaviour
@@ -22,7 +24,13 @@ namespace AuthoringAndMono
     {
         public override void Bake(CardCreatorAuthoring authoring)
         {
-            throw new System.NotImplementedException();
+            Entity cardCreatorEntity = GetEntity(TransformUsageFlags.None); //no need for transform
+            AddComponent(cardCreatorEntity, new HeroCardCreator
+            {
+                HeroCardPrefab = GetEntity(authoring.HeroCardPrefab, TransformUsageFlags.Dynamic),
+                Heroes_JSON_PATH = new FixedString128Bytes(authoring.Heroes_JSON_PATH)
+            }); 
+            Debug.Log($"added HeroCardCreator component with Heroes_JSON_PATH = \"{new FixedString128Bytes(authoring.Heroes_JSON_PATH)}\" to cardCreatorEntity");
         }
     }
 }
--- Code/Here-to-slay/Assets/Scripts/Components/HeroCardCreator.cs
+++ Code/Here-to-slay/Assets/Scripts/Components/HeroCardCreator.cs
@@ -1,11 +1,13 @@
 using Unity.Entities;
 using Components.Enums;
+using Unity.Collections;
 
 namespace Components
 {
     public struct HeroCardCreator : IComponentData
     {
         public Entity HeroCardPrefab;
+        public FixedString128Bytes Heroes_JSON_PATH;
         //any other instantiation data if needed
     }
 }
